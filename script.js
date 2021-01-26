35c60c5c2e6a07cd7fa9a8346044ad0192f64a00
mdzam18 <mdzam18@freeuni.edu.ge>
2021-01-26 22:17:23 +0400

    add javascript

--- script.js
+++ script.js
@@ -120,11 +120,15 @@ async function updateImage2(info1, info2, info3, classname, buttonName1, buttonN
 
 async function showMoviesList() {
     let curr = document.getElementsByClassName("content")[0];
+    let urlOfMovie1 = '#movieInfo?id=' + allInfo[0].id;
+    let urlOfMovie2 = '#movieInfo?id=' + allInfo[1].id;
+    let urlOfMovie3 = '#movieInfo?id=' + allInfo[2].id;
+    let urlOfMovie4 = '#movieInfo?id=' + allInfo[3].id;
     curr.innerHTML = `<div class="content">
         <div class="movie-info">
             <div class="upper-half">
-                <img class="image4"
-                     src=${allInfo[0].image}>
+                <a href=${urlOfMovie1}><img class="image4"
+                     src=${allInfo[0].image}></a>
                 <div class = "description">
                     <h3>${allInfo[0].title}</h3>
                     <p>${allInfo[0].plot}</p>
@@ -136,8 +140,8 @@ async function showMoviesList() {
         </div>
          <div class="movie-info">
             <div class="upper-half">
-                <img class="image4"
-                     src=${allInfo[1].image}>
+                <a href=${urlOfMovie2}><img class="image4"
+                     src=${allInfo[1].image}></a>
                 <div class = "description">
                     <h3>${allInfo[1].title}</h3>
                     <p>${allInfo[1].plot}</p>
@@ -149,8 +153,8 @@ async function showMoviesList() {
         </div>
          <div class="movie-info">
             <div class="upper-half">
-                <img class="image4"
-                     src=${allInfo[2].image}>
+                <a href=${urlOfMovie3}><img class="image4"
+                     src=${allInfo[2].image}></a>
                 <div class = "description">
                     <h3>${allInfo[2].title}</h3>
                     <p>${allInfo[2].plot}</p>
@@ -162,8 +166,8 @@ async function showMoviesList() {
         </div>
          <div class="movie-info">
             <div class="upper-half">
-                <img class="image4"
-                     src=${allInfo[3].image}>
+                <a href=${urlOfMovie4}><img class="image4"
+                     src=${allInfo[3].image}></a>
                 <div class = "description">
                     <h3>${allInfo[3].title}</h3>
                     <p>${allInfo[3].plot}</p>
